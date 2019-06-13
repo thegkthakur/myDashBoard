7555ec0fd877208e30616b204efea03a2df20dae
thegkthakur <t.gov.92@gmail.com>
2019-06-13 20:38:01 +0530

    initial commit

---
+++ details.js
@@ -0,0 +1,12 @@
+'use strict';
+let result = function(e) {
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+}
+
+function passvalues() {
+
+    localStorage.setItem("textvalues", username);
+    return false;
+}
